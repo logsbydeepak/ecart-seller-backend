7b886e8992fa6549897bf624b45ffdec01067c32
Deepak Kumar <git.logsbydeepak@gmail.com>
2022-03-05 14:24:20 +0000

    add error in gql

--- src/gql/user/resolver/create.user.resolver.ts
+++ src/gql/user/resolver/create.user.resolver.ts
@@ -4,7 +4,19 @@ export const createUser = async (
   _: any,
   args: { name: string; email: string; password: string }
 ) => {
-  const newUser = await new UserModel(args);
-  newUser.save();
-  return { name: newUser.name, email: newUser.email };
+  try {
+    if (!args.name || !args.email || !args.password) {
+      return { __typename: "Error", message: "Invalid args" };
+    }
+
+    const newUser = await new UserModel(args);
+    newUser.save();
+    return {
+      __typename: "CreateUser",
+      name: newUser.name,
+      email: args.email,
+    };
+  } catch (e: any) {
+    return { __typename: "Error", message: "Something went wrong" };
+  }
 };
