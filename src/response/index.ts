0688e969850e60a4e01864d344b44588f732e50e
Deepak Kumar <git.logsbydeepak@gmail.com>
2022-03-15 13:16:59 +0000

    create error response object

---
+++ src/response/index.ts
@@ -0,0 +1,28 @@
+import { ErrorResponse as ErrorResponseType } from "types/graphql";
+import errorData from "./error.data.json";
+
+export const ErrorObject = (messageTypeCode: string, messageCode: number) => ({
+  ErrorObject: {
+    messageTypeCode,
+    messageCode,
+  },
+});
+
+export const ErrorResponse = (
+  messageTypeCode: string,
+  messageCode: number
+): ErrorResponseType => {
+  const responseData = errorData.find(
+    (data: any) => data.messageTypeCode === messageTypeCode
+  )!;
+
+  const responseMessage = responseData.response.find(
+    (data) => data.messageCode === messageCode
+  )!;
+
+  return {
+    title: responseData.messageType,
+    message: responseMessage.message,
+    statusCode: responseMessage.statusCode,
+  };
+};
