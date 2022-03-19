bf4b6b76781c69e8b32d46078ce872b0b8943bd7
Deepak Kumar <git.logsbydeepak@gmail.com>
2022-03-19 08:51:39 +0000

    create product gql schema

--- src/gql/gql.schema.ts
+++ src/gql/gql.schema.ts
@@ -5,9 +5,10 @@ import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
 const allSchema = resolve(__dirname + "/gql.schema.gql");
 const userSchema = resolve(__dirname + "/user/user.schema.gql");
 const sessionSchema = resolve(__dirname + "/session/session.schema.gql");
+const productSchema = resolve(__dirname + "/product/product.schema.gql");
 
 export const gqlSchema = loadSchemaSync(
-  [allSchema, userSchema, sessionSchema],
+  [allSchema, userSchema, sessionSchema, productSchema],
   {
     loaders: [new GraphQLFileLoader()],
   }
