8ce7de84f612a21ade6c19f9a80ee15f560f11a0
Deepak Kumar <git.logsbydeepak@gmail.com>
2022-03-25 14:55:37 +0000

    fix typo

--- src/gql/gql.resolver.ts
+++ src/gql/gql.resolver.ts
@@ -1,7 +1,7 @@
 import { GQLContext } from "types";
 import { Resolvers } from "types/graphql";
 import { userQueryResolver, userMutationResolver } from "./user/user.resolver";
-import { sessionMutationResolver } from "./session/sesion.resolver";
+import { sessionMutationResolver } from "./session/session.resolver";
 import {
   productMutationResolver,
   productQueryResolver,
