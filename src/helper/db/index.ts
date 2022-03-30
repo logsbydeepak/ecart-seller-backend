4ca5ce36607b775361704e05aa31f10b6965ec63
Deepak Kumar <git.logsbydeepak@gmail.com>
2022-03-30 17:30:47 +0000

    fix db connection

--- src/helper/db/index.ts
+++ src/helper/db/index.ts
@@ -1,23 +1,22 @@
 import { DB_URL_MAIN, DB_URL_SELLER } from "config";
 import mongoose from "mongoose";
-import { dbUserExist } from "./user.db";
 
 export * from "./token.db";
 export * from "./user.db";
 
 export const establishDBConnection = (DB_URL: string, DBName: string) => {
-  mongoose.createConnection(DB_URL);
+  const db = mongoose.createConnection(DB_URL);
 
-  mongoose.connection.on("open", () => {
-    console.log(`${DBName} | DB connection established successfully`);
+  db.on("open", () => {
+    console.log(`${DBName} || DB connection established successfully`);
   });
 
-  mongoose.connection.on("error", () => {
-    console.log(`${DBName} | ${DB_URL} | Error establishing DB connection`);
+  db.on("error", () => {
+    console.log(`${DBName} || ${DB_URL} || Error establishing DB connection`);
     process.exit(1);
   });
 
-  return mongoose.connection;
+  return db;
 };
 
 export const DB_MAIN = establishDBConnection(DB_URL_MAIN as string, "main");
