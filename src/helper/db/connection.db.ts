34af496b5192c80204374a9eae636e040198e558
Deepak Kumar <git.logsbydeepak@gmail.com>
2022-03-31 10:16:52 +0000

    update npm

--- src/helper/db/connection.db.ts
+++ src/helper/db/connection.db.ts
@@ -2,11 +2,11 @@ import { Connection } from "mongoose";
 
 export const verifyConnection = (DBConnection: Connection, DBName: string) => {
   DBConnection.on("open", () => {
-    console.log(`${DBName} || DB connection established successfully`);
+    console.log(`[ ${DBName} ] DB connection established successfully`);
   });
 
   DBConnection.on("error", () => {
-    console.log(`${DBName} || Error establishing DB connection`);
+    console.log(`[ ${DBName} ] Error establishing DB connection`);
     process.exit(1);
   });
 };
