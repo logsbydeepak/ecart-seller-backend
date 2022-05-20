1d69e9fbff983b9872c369c1d3201101079da89a
Deepak Kumar <git.logsbydeepak@gmail.com>
2022-05-20 06:18:00 +0000

    refresh token without access token

--- src/gql/session/resolver/update.session.resolver.ts
+++ src/gql/session/resolver/update.session.resolver.ts
@@ -1,11 +1,9 @@
 import {
   accessTokenGenerator,
-  accessTokenValidator,
   refreshTokenValidator,
 } from "~/helper/token.helper";
 
 import { ResolveMutation } from "~/types";
-import { dbTokenExist } from "~/db/query/token.query";
 import { validateEmpty } from "~/helper/validator.helper";
 import { generateDecryption } from "~/helper/security.helper";
 import { removeRefreshTokenCookie } from "~/helper/cookie.helper";
@@ -17,22 +15,10 @@ const updateSession: ResolveMutation<"updateSession"> = async (
   { req, res }
 ) => {
   try {
-    const accessToken: string = validateEmpty(
-      req.headers["x-access-token"],
-      "TOKEN_PARSE",
-      "access token is required"
-    );
-
     const refreshToken: string = validateEmpty(
       req.cookies.refreshToken,
       "TOKEN_PARSE",
       "access token is required"
-    );
-
-    const accessTokenDecryption: string = generateDecryption(
-      accessToken,
-      "TOKEN_PARSE",
-      "invalid access token"
     );
 
     const refreshTokenDecryption: string = generateDecryption(
@@ -41,31 +27,16 @@ const updateSession: ResolveMutation<"updateSession"> = async (
       "invalid refresh token"
     );
 
-    const accessTokenData = accessTokenValidator(accessTokenDecryption);
     const refreshTokenData = refreshTokenValidator(refreshTokenDecryption);
-
-    if (accessTokenData === null) {
-      throw ErrorObject("TOKEN_PARSE", "invalid access token");
-    }
 
     if (refreshTokenData === null) {
       throw ErrorObject("TOKEN_PARSE", "invalid refresh token");
-    }
-
-    if (accessTokenData !== "TokenExpiredError") {
-      throw ErrorObject("TOKEN_PARSE", "access token is not expired");
     }
 
     if (refreshTokenData === "TokenExpiredError") {
       removeRefreshTokenCookie(res);
       throw ErrorObject("TOKEN_PARSE", "refresh token expired");
     }
-
-    await dbTokenExist(
-      { owner: refreshTokenData.id, token: accessToken },
-      "TOKEN_PARSE",
-      "invalid access token"
-    );
 
     const newAccessToken = accessTokenGenerator(refreshTokenData.id);
 
