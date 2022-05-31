ea654618518b06c16732584e27189e8b3564f665
Deepak Kumar <git.logsbydeepak@gmail.com>
2022-05-31 11:55:51 +0000

    remove validator from use schema

--- src/db/schema/user.schema.ts
+++ src/db/schema/user.schema.ts
@@ -1,14 +1,7 @@
 import { Schema } from "mongoose";
-import { generateHashAndSalt } from "~/helper/security.helper";
-
-import {
-  validateEmail,
-  validateEmpty,
-  validatePassword,
-} from "~/helper/validator.helper";
 
 import { UserModelType } from "~/types";
-import { dbEmailExist } from "../query/user.query";
+import { generateHashAndSalt } from "~/helper/security.helper";
 
 const defaultProperty = {
   required: true,
@@ -23,22 +16,6 @@ const UserSchema = new Schema<UserModelType>({
 });
 
 UserSchema.post("validate", async function () {
-  this.firstName = validateEmpty(
-    this.firstName,
-    "BODY_PARSE",
-    "firstName is required"
-  );
-  this.lastName = validateEmpty(
-    this.lastName,
-    "BODY_PARSE",
-    "lastName is required"
-  );
-  this.email = validateEmail(this.email);
-
-  if (!this.isModified("email")) return;
-  await dbEmailExist(this.email);
-
-  this.password = validatePassword(this.password);
   if (!this.isModified("password")) return;
   this.password = await generateHashAndSalt(this.password);
   return;
--- src/gql/user/resolver/create.user.resolver.ts
+++ src/gql/user/resolver/create.user.resolver.ts
@@ -3,9 +3,15 @@ import {
   refreshTokenGenerator,
 } from "~/helper/token.helper";
 
+import {
+  validateBody,
+  validateEmail,
+  validateEmpty,
+  validatePassword,
+} from "~/helper/validator.helper";
+
 import { ResolveMutation } from "~/types";
 import { UserModel } from "~/db/model.db";
-import { validateBody } from "~/helper/validator.helper";
 import { handleCatchError } from "~/helper/response.helper";
 import { setRefreshTokenCookie } from "~/helper/cookie.helper";
 
@@ -13,7 +19,22 @@ const createUser: ResolveMutation<"createUser"> = async (_, args, { res }) => {
   try {
     const bodyData = validateBody(args, 4);
 
-    const newUser = new UserModel(bodyData);
+    const firstName = validateEmpty(
+      bodyData.firstName,
+      "BODY_PARSE",
+      "firstName is required"
+    );
+    const lastName = validateEmpty(
+      bodyData.lastName,
+      "BODY_PARSE",
+      "lastName is required"
+    );
+    const email = validateEmail(bodyData.email);
+    const password = validatePassword(bodyData.password);
+
+    const newUser = new UserModel({ firstName, lastName, email, password });
+    await newUser.save();
+
     const newUserId = newUser._id;
 
     const accessToken = accessTokenGenerator(newUserId);
