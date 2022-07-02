48bdb60515e78241e9589b59657b17ef82a1e5ad
Deepak Kumar <git.logsbydeepak@gmail.com>
2022-07-02 11:46:19 +0000

    add removeUserPicture

---
+++ src/gql/resolvers/removeUserPicture.ts
@@ -0,0 +1,37 @@
+import { handleCatchError } from "~/helper/response.helper";
+import { validateEmpty } from "~/helper/validator.helper";
+
+import { GQLResolvers } from "~/types/index";
+import { dbReadUserById } from "~/db/query/user.query";
+import cloudinary from "~/config/cloudinary.config";
+
+const removeUserPicture: GQLResolvers = {
+  Mutation: {
+    removeUserPicture: async (
+      _parent,
+      args,
+      { req, validateTokenMiddleware, validatePasswordMiddleware }
+    ) => {
+      try {
+        const { userId } = await validateTokenMiddleware(req);
+        const dbUser = await dbReadUserById<"removeUserPicture">(userId, {
+          __typename: "TokenError",
+          type: "TokenUserDoNotExistError",
+          message: "user do not exist",
+        });
+
+        dbUser.picture = "default";
+        await dbUser.save();
+
+        return {
+          __typename: "SuccessResponse",
+          message: 'picture" has been removed',
+        };
+      } catch (error) {
+        return handleCatchError(error);
+      }
+    },
+  },
+};
+
+export default removeUserPicture;
