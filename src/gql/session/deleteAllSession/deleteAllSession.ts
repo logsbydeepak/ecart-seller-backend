2f56e05769c7c447d04315688ca921cc7945f57d
Deepak Kumar <git.logsbydeepak@gmail.com>
2022-07-28 15:01:07 +0000

    fix deleteAllSession

--- src/gql/session/deleteAllSession/deleteAllSession.ts
+++ src/gql/session/deleteAllSession/deleteAllSession.ts
@@ -1,30 +1,58 @@
-import { GQLResolvers } from "~/types/graphqlHelper";
-
+import * as yup from "yup";
+import { UserModel } from "~/db/model.db";
 import { redisClient } from "~/config/redis.config";
+import { GQLResolvers } from "~/types/graphqlHelper";
 import { handleCatchError } from "~/helper/response.helper";
+import { validatePassword } from "~/helper/security.helper";
+import { TokenUserDoNotExistError } from "~/helper/error.helper";
+import { password, validateArgs } from "~/helper/validator.helper";
+
+const validateSchema = yup.object({
+  currentPassword: password,
+});
 
 const DeleteAllSession: GQLResolvers = {
   Mutation: {
     deleteAllSession: async (
       _parent,
       args,
-      { req, validateTokenMiddleware, validatePasswordMiddleware }
+      { req, validateTokenMiddleware }
     ) => {
       try {
-        const { userId } = await validateTokenMiddleware(req);
-
-        await validatePasswordMiddleware<"deleteAllSession">(
-          args.currentPassword,
-          userId,
-          {
-            __typename: "DeleteAllSessionCredentialError",
-            message: "password is required",
-          },
-          {
-            __typename: "DeleteAllSessionCredentialError",
-            message: "invalid password",
-          }
+        const { argsData, argsError } = await validateArgs(
+          validateSchema,
+          args
         );
+
+        if (argsError) {
+          if (argsError.field !== "currentPassword") {
+            throw Error();
+          }
+
+          return {
+            __typename: "DeleteAllSessionArgsError",
+            field: argsError.field,
+            message: argsError.message,
+          };
+        }
+
+        const { tokenData, tokenError } = await validateTokenMiddleware(req);
+        if (tokenError) return tokenError;
+        const { userId, token } = tokenData;
+
+        const dbUser = await UserModel.findById(userId);
+        if (!dbUser) return TokenUserDoNotExistError;
+
+        const isValidPassword = validatePassword({
+          rawPassword: argsData.currentPassword,
+          dbPassword: dbUser.password,
+        });
+
+        if (!isValidPassword)
+          return {
+            __typename: "InvalidCredentialError",
+            message: "invalid credential",
+          };
 
         await redisClient.DEL(userId);
 
@@ -33,7 +61,7 @@ const DeleteAllSession: GQLResolvers = {
           message: "logout from all device successfully",
         };
       } catch (error) {
-        return handleCatchError(error);
+        return handleCatchError();
       }
     },
   },
