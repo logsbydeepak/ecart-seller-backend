d09a65e34faefd62181caaa53ce520530f4f6b1b
Deepak Kumar <git.logsbydeepak@gmail.com>
2022-07-28 14:32:28 +0000

    fix createSession

--- src/gql/session/createSession/createSession.ts
+++ src/gql/session/createSession/createSession.ts
@@ -1,69 +1,69 @@
+import * as yup from "yup";
+import { UserModel } from "~/db/model.db";
 import { redisClient } from "~/config/redis.config";
-import { dbReadUserByEmail } from "~/db/query/user.query";
-
 import { GQLResolvers } from "~/types/graphqlHelper";
-import { Mutation, MutationCreateSessionArgs } from "~/types/graphql";
-
+import { InvalidCredentialError } from "~/types/graphql";
 import { tokenGenerator } from "~/helper/token.helper";
 import { handleCatchError } from "~/helper/response.helper";
-import { validateHashAndSalt } from "~/helper/security.helper";
-import { validateEmail, validatePassword } from "~/helper/validator.helper";
+import { validatePassword } from "~/helper/security.helper";
+import { email, password, validateArgs } from "~/helper/validator.helper";
 
-const invalidCredentialError: Mutation["createSession"] = {
-  __typename: "CreateSessionCredentialError",
-  message: "invalid email or password",
+const InvalidCredentialError: InvalidCredentialError = {
+  __typename: "InvalidCredentialError",
+  message: "invalid credential",
 };
+
+const validateSchema = yup.object({
+  email,
+  password,
+});
 
 const CreateSession: GQLResolvers = {
   Mutation: {
     createSession: async (_parent, args) => {
       try {
-        const { email, password } = validateCreateSessionArgs(args);
-
-        const { _id: dbUserId, password: dbPassword } = await dbReadUserByEmail(
-          email
+        const { argsData, argsError } = await validateArgs(
+          validateSchema,
+          args
         );
 
-        await validateHashAndSalt<"createSession">(
-          password,
-          dbPassword,
-          invalidCredentialError
-        );
+        if (argsError) {
+          if (argsError.field !== "email" && argsError.field !== "password") {
+            throw Error();
+          }
 
-        const token = tokenGenerator(dbUserId);
-        await redisClient.SADD(dbUserId.toString(), token);
+          return {
+            __typename: "CreateSessionArgsError",
+            field: argsError.field,
+            message: argsError.message,
+          };
+        }
+
+        const dbUser = await UserModel.findOne({
+          email: argsData.email,
+        });
+
+        if (!dbUser) return InvalidCredentialError;
+
+        const isValidPassword = validatePassword({
+          rawPassword: argsData.password,
+          dbPassword: dbUser.password,
+        });
+
+        if (!isValidPassword) return InvalidCredentialError;
+
+        const token = tokenGenerator(dbUser._id);
+        await redisClient.SADD(dbUser._id.toString(), token);
 
         return {
           __typename: "Token",
           token,
         };
       } catch (error) {
-        return handleCatchError(error);
+        return handleCatchError();
       }
     },
   },
-};
-
-const validateCreateSessionArgs = (args: MutationCreateSessionArgs) => {
-  const email = validateEmail<"createSession">(
-    args.email,
-    {
-      __typename: "CreateSessionCredentialError",
-      message: "email is required",
-    },
-    invalidCredentialError
-  );
-
-  const password = validatePassword<"createSession">(
-    args.password,
-    {
-      __typename: "CreateSessionCredentialError",
-      message: "password is required",
-    },
-    invalidCredentialError
-  );
-
-  return { email, password };
 };
 
 export default CreateSession;
